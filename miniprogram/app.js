1b1b06970c19c624c03c5f1f547d2d6e6c58d509
likanghua <lkh19890913@163.com>
2021-03-14 23:31:32 +0800

    feat: add mobx-miniprogram

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,3 +1,9 @@
+// import { promisifyAll, promisify } from 'miniprogram-api-promise';
+
+// const wxp = {}
+// // promisify all wx's api
+// promisifyAll(wx, wxp)
+
 //app.js
 App({
   onLaunch: function () {
@@ -9,7 +15,7 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
+        env: 'bigfish-1gaq5795042c9778',
         traceUser: true,
       })
     }
